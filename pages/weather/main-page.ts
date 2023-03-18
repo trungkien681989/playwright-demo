95e263456a0eda48f902f970f9fe27724e06917f
Kien <trungkien681989@gmail.com>
2023-03-18 13:29:00 +0700

    Add base pages

---
+++ pages/weather/main-page.ts
@@ -0,0 +1,29 @@
+import { Page } from '@playwright/test';
+import { BasePage } from '../base/base-page';
+
+export class MainPage extends BasePage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  /* ============ Elements =============== */
+
+  readonly headerElements = {
+    tab: (name: string) => `#regionHeader [id*="LocalsuiteNav-header"] a span >> text=${name}`,
+    savedLocation: (name: string) => `#regionHeader [id*="SavedLocations-header"] a span >> text=${name}`,
+  }
+
+  /* ============ Methods =============== */
+
+  async goToTab(name: string) {
+    await this.waitAndClick(this.headerElements.tab(name));
+  }
+
+  /*==================Verification==============*/
+
+  async verifySavedLocation(location: string) {
+    await this.verifyElementVisible(this.headerElements.savedLocation(location));
+  }
+}
