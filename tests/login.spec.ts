0027288d438d236130b0f2c0db7298bbb92ae1df
Kien <trungkien681989@gmail.com>
2025-02-25 22:28:00 +0700

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,7 +5,7 @@ import { ValidLoginData } from '../test-data/login-data';
 import { WelcomePage } from '../pages/welcome/welcome-page';
 
 ValidLoginData.array.forEach(data => {
-    test(`Login with valid email ${data.email} and valid password ${data.password} @smoke`, async ({ browser }) => {
+    test(`Login with valid email ${data.email} and valid password ${data.password} @smoke @regression`, async ({ browser }) => {
         const context = await browser.newContext();
         const page = await context.newPage();
         const loginPage = new LoginPage(page);
