edb768e27f7549375dcee29ac8206b365edf2c5f
Kien <trungkien681989@gmail.com>
2025-02-25 23:24:08 +0700

    Create signup-data.ts

---
+++ test-data/signup-data.ts
@@ -0,0 +1,9 @@
+export const SignupData = {
+    "array": [
+        {
+            "fullName": "Kevin Bui",
+            "email": "trungkien681989@gmail.com",
+            "password": "P@$$word123",
+        }
+    ]
+}
