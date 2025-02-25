68b0d3ef91653a581c5ba4b96192d8d9df2f0f88
Kien <trungkien681989@gmail.com>
2025-02-25 23:24:02 +0700

    Update login.spec.ts

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,11 @@
 import { test } from '@playwright/test';
 import ENV from '../helper/env-config';
-import { LoginPage } from '../pages/login/login-page';
 import { ValidLoginData } from '../test-data/login-data';
+import { LoginPage } from '../pages/login/login-page';
 import { WelcomePage } from '../pages/welcome/welcome-page';
 
 ValidLoginData.array.forEach(data => {
-    test(`Login with valid email ${data.email} and valid password ${data.password} @smoke @regression`, async ({ browser }) => {
+    test(`Test successful login with valid email ${data.email} and valid password ${data.password} @smoke @regression`, async ({ browser }) => {
         const context = await browser.newContext();
         const page = await context.newPage();
         const loginPage = new LoginPage(page);
