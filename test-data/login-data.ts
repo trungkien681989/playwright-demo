ddd663e47ae61b194d185cdd10caa7fae05b3ffa
Kien <trungkien681989@gmail.com>
2025-02-25 22:35:44 +0700

    Update login-data.ts

--- test-data/login-data.ts
+++ test-data/login-data.ts
@@ -2,7 +2,7 @@ export const ValidLoginData = {
     "array": [
         {
             "email": "trungkien681989@gmail.com",
-            "password": "en-SG",
+            "password": "P@$$word123",
         }
     ]
 }
