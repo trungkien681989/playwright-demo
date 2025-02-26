95f55d9bd10e85e27871b281d3ae6018933cae20
Kien <trungkien681989@gmail.com>
2025-02-26 11:30:40 +0700

    Update test data

--- test-data/login/valid-login-data.ts
+++ test-data/login/valid-login-data.ts
@@ -3,6 +3,10 @@ const ValidLoginDataTest = {
         {
             "email": "test123@gmail.com",
             "password": "P@$$word123",
+        },
+        {
+            "email": "test456@gmail.com",
+            "password": "P@$$word456",
         }
     ]
 }
@@ -12,6 +16,10 @@ const ValidLoginDataProd = {
         {
             "email": "trungkien681989@gmail.com",
             "password": "P@$$word123",
+        },
+        {
+            "email": "nguyenthihongnhung20101989@gmail.com",
+            "password": "P@$$word456",
         }
     ]
 }
