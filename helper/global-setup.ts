4463644407982c2fe857bdafd6c1fcab5bb496e1
Kien <49904115+trungkien681989@users.noreply.github.com>
2025-02-27 15:18:53 +0700

    Update global-setup.ts

--- helper/global-setup.ts
+++ helper/global-setup.ts
@@ -2,7 +2,6 @@ import { FullConfig } from '@playwright/test';
 import dotenv from 'dotenv';
 
 async function globalSetup(config: FullConfig) {
-  // Handle environment config
   if (process.env.test_env) {
     dotenv.config({
       path: `./environments/.env.${process.env.test_env}`,
